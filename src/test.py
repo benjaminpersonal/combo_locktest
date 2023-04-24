6c20d54f36f600d995d0eeb001bb17ab1a057255
benjaminpersonal <131632003+benjaminpersonal@users.noreply.github.com>
2023-04-23 20:46:56 -0600

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,7 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+
+@cocotb.test()
+async def test_my_design(dut):
+    dut._log.info("start")
