efced730bfe6e1fda69847af9be498a51154b69b
benjaminpersonal <131632003+benjaminpersonal@users.noreply.github.com>
2023-04-28 21:31:40 -0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
     dut._log.info("start")
-    clock = Clock(dut.CLK, 7, units="ms")
+    clock = Clock(dut.CLK, 6, units="ms")
     cocotb.start_soon(clock.start())
     if dut.OUT[0].value == '1':
         dut.code[0].value = 1
