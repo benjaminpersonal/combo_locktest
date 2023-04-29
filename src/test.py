5a77d23c671740d1fd72a056829dee5354a1ee06
benjaminpersonal <131632003+benjaminpersonal@users.noreply.github.com>
2023-04-28 21:39:33 -0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -7,5 +7,6 @@ async def test_my_design(dut):
     dut._log.info("start")
     clock = Clock(dut.CLK, 7, units="ms")
     cocotb.start_soon(clock.start())
-    if dut.OUT[0].value == '1':
+    dut._log.info()
+    if dut.OUTPUT[0].value == '1':
         dut.code[0].value = 1
